7bc57e950bc9c76da5eb6f164344c259da146a04
dnnrnbyn <116276006+dnnrnbyn@users.noreply.github.com>
2022-11-25 16:16:13 +0800

    Add files via upload

---
+++ CreatingTextFile/FrmFileName.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CreatingTextFile
+{
+    partial class FrmFileName
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOkay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(12, 47);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(248, 20);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter File Name";
+            // 
+            // btnOkay
+            // 
+            this.btnOkay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btnOkay.Location = new System.Drawing.Point(102, 86);
+            this.btnOkay.Name = "btnOkay";
+            this.btnOkay.Size = new System.Drawing.Size(72, 26);
+            this.btnOkay.TabIndex = 2;
+            this.btnOkay.Text = "Okay";
+            this.btnOkay.UseVisualStyleBackColor = true;
+            this.btnOkay.Click += new System.EventHandler(this.btnOkay_Click);
+            // 
+            // FrmFileName
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 124);
+            this.Controls.Add(this.btnOkay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "FrmFileName";
+            this.Text = "FrmFileName";
+            this.Load += new System.EventHandler(this.FrmFileName_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOkay;
+    }
+}
